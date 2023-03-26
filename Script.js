2e68d709c40ca0c9910241022ed6094224af332b
pedro <pedrohenriquehoppe6@gmail.com>
2023-03-26 15:40:46 -0300

    9g9ug

--- Script.js
+++ Script.js
@@ -5,47 +5,47 @@ const Endere_ = document.getElementById('Endereço');
 
 const Bnt_cadastra_aluno = document.getElementById('bnt_cadastrar');
 const LimparCampo = document.getElementById('bnt_clear_fild');
-const Form_ = document.getElementById('id_form');
+const Form_ = document.querySelector('form');
 
 
-function ClearCampo() {
-      document.getElementById('id_form').reset();
-}
+Bnt_cadastra_aluno.addEventListener('click', function(e) {
+      e.preventDefault();
+      let cont_verifica = 0;
 
-function verifica() {
       if(InputMatricula.value == "") {
-            document.getElementById('Matricula').classList.add("ValidaInputError");
+            document.getElementById('Matricula').classList.add('ValidaInputError');
       } else {
-            document.getElementById('Matricula').classList.remove("ValidaInputError");
-            document.getElementById('Matricula').classList.add("ValidaInput");
+            document.getElementById('Matricula').classList.remove('ValidaInputError');
+            document.getElementById('Matricula').classList.add('ValidaInput');
+            cont_verifica++;
       }
 
       if(inputNome.value == "") {
-            document.getElementById('nome').classList.add("ValidaInputError");
+            document.getElementById('nome').classList.add('ValidaInputError');
       } else {
-            document.getElementById('nome').classList.remove("ValidaInputError");
-            document.getElementById('nome').classList.add("ValidaInput");
+            document.getElementById('nome').classList.remove('ValidaInputError');
+            document.getElementById('nome').classList.add('ValidaInput');
+            cont_verifica++;
       }
 
       if(Telefone.value == "") {
-            document.getElementById('Telefone').classList.add("ValidaInputError");
+            document.getElementById('Telefone').classList.add('ValidaInputError');
       } else {
-            document.getElementById('Telefone').classList.remove("ValidaInputError");
+            document.getElementById('Telefone').classList.remove('ValidaInputError');
+            document.getElementById('Telefone').classList.add('ValidaInput');
+            cont_verifica++;
       }
 
       if(Endere_.value == "") {
-            document.getElementById('Endereço').classList.add("ValidaInputError");
+            document.getElementById('Endereço').classList.add('ValidaInputError');
       } else {
-            document.getElementById('Endereço').classList.remove("ValidaInputError");
-            document.getElementById('Endereço').classList.add("ValidaInput");
+            document.getElementById('Endereço').classList.remove('ValidaInputError');
+            document.getElementById('Endereço').classList.add('ValidaInput');
+            cont_verifica++;
       }
-}
 
-
-Bnt_cadastra_aluno.addEventListener('click', function(event) {
-      event.preventDefault();
-      verifica();
+      if(cont_verifica == 4) {
+            alert('Tudo ok');
+      }
 
 });
-
-
